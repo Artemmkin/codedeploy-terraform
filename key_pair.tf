e8956624da0e8a82047d9848cd9fe16d25c8aa2b
Greg Uctum <greg.uctum@amway.com>
2020-03-10 10:40:32 -0400

    updated to terraform v.012

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "main" {
   key_name   = "code-deploy-demo"
-  public_key = "${file(var.public_key_path)}"
+  public_key = file(var.public_key_path)
 }
--- roles.tf
+++ roles.tf
@@ -24,7 +24,7 @@ EOF
 # attach AWS managed policy called AWSCodeDeployRole
 # required for deployments which are to an EC2 compute platform
 resource "aws_iam_role_policy_attachment" "codedeploy_service" {
-  role       = "${aws_iam_role.codedeploy_service.name}"
+  role       = aws_iam_role.codedeploy_service.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
 
@@ -53,11 +53,11 @@ EOF
 
 # provide ec2 access to s3 bucket to download revision. This role is needed by the CodeDeploy agent on EC2 instances.
 resource "aws_iam_role_policy_attachment" "instance_profile_codedeploy" {
-  role       = "${aws_iam_role.instance_profile.name}"
+  role       = aws_iam_role.instance_profile.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
 }
 
 resource "aws_iam_instance_profile" "main" {
   name = "codedeploy-instance-profile"
-  role = "${aws_iam_role.instance_profile.name}"
+  role = aws_iam_role.instance_profile.name
 }
